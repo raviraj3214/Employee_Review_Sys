ab923cff714a7d831732bffe74fd9f074c9b1201
Ravi Raj <103103100+raviraj3214@users.noreply.github.com>
2023-08-01 00:48:41 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
-mongoose.connect('mongodb://127.0.0.1:27017/employeereviewsystem')
+mongoose.connect('process.env.DB');
       
 
 const db = mongoose.connection
